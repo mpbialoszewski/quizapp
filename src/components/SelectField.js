8627454aa15742380ddf8901bc1db5a1aa13e4cd
mpbialoszewski <m.p.bialoszewski@outlook.com>
2022-07-05 21:04:38 +0100

    Mapping over the props of names of API elements

--- src/components/SelectField.js
+++ src/components/SelectField.js
@@ -3,19 +3,25 @@ import {Box, FormControl, InputLabel, Menu, MenuItem, Select} from "@mui/materia
 
 
 const SelectField = props => {
-    const {label} = props
+    const {label ,options} = props
     const [value, setValue] = useState("")
 
-    const handleChange = () => { }
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
     return (
         <Box mt={3} width={"100%"}>
             <FormControl fullWidth={true}>
                 <InputLabel>{label}</InputLabel>
 
                 <Select value={value} label={label} onChange={handleChange}>
-                    <MenuItem>Option 1</MenuItem>
-                    <MenuItem>Option 2</MenuItem>
-                    <MenuItem>Option 3</MenuItem>
+                    {options.map(({ id,name }) =>(
+
+                            <MenuItem value={id} key={id}>
+                                {name}
+                            </MenuItem>
+
+                        ))}
                 </Select>
             </FormControl>
         </Box>
