048ebd9170a0b6d0837f95a3b7ac95d8d5d32eea
mpbialoszewski <m.p.bialoszewski@outlook.com>
2022-07-05 19:32:57 +0100

    Text field

---
+++ src/components/TextFieldComp.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Box, FormControl, TextField} from "@mui/material";
+
+const TextFieldComp = () => {
+
+    const handleChange = () => {}
+
+    return (
+        <Box mt={3} width="100%">
+            <FormControl fullWidth size={"small"}>
+                <TextField
+                    onChange={handleChange}
+                    variant={"outlined"}
+                    label={"Amount of Questions"}
+                    type={"number"}
+                    size={"small"}
+                />
+            </FormControl>
+        </Box>
+    );
+};
+
+export default TextFieldComp;
