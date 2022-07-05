bb5c25dd1a8badd95561e0b24ce095835a3ce7ae
mpbialoszewski <m.p.bialoszewski@outlook.com>
2022-07-05 21:03:54 +0100

    Displaying difficulty and type options

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -23,15 +23,28 @@ const Settings = () => {
         )
     }
 
+    const difficultyOptions = [
+        { id: "easy", name: "Easy"},
+        { id: "medium", name: "Kinda hard"},
+        { id: "hard", name: "Extremely Difficult"}
+    ]
+
+    const typeOptions = [
+        { id: "multiple", name: "Multiple Choice"},
+        { id: "boolean", name: "True/False"}
+
+    ]
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <SelectField label={"Category"} />
-                <SelectField label={"Difficulty"} />
-                <SelectField label={"Type"} />
+                <SelectField options={response.trivia_categories} label={"Category"} />
+                <SelectField options={difficultyOptions} label={"Difficulty"} />
+                <SelectField options={typeOptions} label={"Type"} />
                 <TextFieldComp />
                 <Box mt={3} width={"100%"}>
                     <Button fullWidth variant={"contained"} type={"submit"}>
