bb3d14cf9344062a4193324b2ac6e7123b2ea37a
mpbialoszewski <m.p.bialoszewski@outlook.com>
2022-07-05 19:32:41 +0100

    Loading data from API, error handling

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import SelectField from "../components/SelectField";
-import {Box, Button} from "@mui/material";
+import {Box, Button, CircularProgress, Typography} from "@mui/material";
+import TextFieldComp from "../components/TextFieldComp";
+import useAxios from "../hooks/useAxios";
 
 const Settings = () => {
+    const { response,error,loading } = useAxios({url: "/api_category.php"})
+
+    if(loading) {
+        return(
+            <Box mt={20}>
+                <CircularProgress />
+            </Box>
+        )
+    }
+
+    if(error){
+        return(
+            <Typography variant={"h6"} mt={20} color="red">
+                Something went wrong
+            </Typography>
+        )
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -13,6 +32,7 @@ const Settings = () => {
                 <SelectField label={"Category"} />
                 <SelectField label={"Difficulty"} />
                 <SelectField label={"Type"} />
+                <TextFieldComp />
                 <Box mt={3} width={"100%"}>
                     <Button fullWidth variant={"contained"} type={"submit"}>
                         Get started!
