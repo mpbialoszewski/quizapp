8fcbbc8fd7c3766716b949481c455c6c661a08bf
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-07-30 10:11:54 +0100

    Initial Question page[0]

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,10 +1,27 @@
+import { Button, Typography } from '@mui/material';
+import {Box } from '@mui/system';
 import React from 'react';
 
 const Questions = () => {
     return (
-        <div>
-            Questions
-        </div>
+        (<Box>
+            <Typography variant='h4'>Question 1</Typography>
+            <Typography mt={5}> This is the question </Typography>
+                <Box mt={2}>
+                    <Button variant='contained'>Answer A</Button>
+                </Box>
+                <Box mt={2}>
+                    <Button variant='contained'>Answer B</Button>
+                </Box>
+                <Box mt={2}>
+                    <Button variant='contained'>Answer C</Button>
+                </Box>
+
+                <Box mt={2}>
+                    <Button>Score  2/6</Button>
+                </Box>
+        </Box>
+        )
     );
 };
 
