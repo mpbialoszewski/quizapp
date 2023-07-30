4757a22920bfc48d01876fe723617a848f4257f2
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-07-30 14:27:24 +0100

    setting dispatch response upon submit[0]

--- src/components/TextFieldComp.js
+++ src/components/TextFieldComp.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Box, FormControl, TextField} from "@mui/material";
+import { useDispatch } from 'react-redux';
+import { handleAmountChange } from "../redux/actions";
 
 const TextFieldComp = () => {
-
-    const handleChange = () => {}
+    const dispatch = useDispatch()
+    const handleChange = (e) => {
+        dispatch(handleAmountChange(e.target.value))
+    }
 
     return (
         <Box mt={3} width="100%">
