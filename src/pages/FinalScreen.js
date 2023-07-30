e48993a7855d0141321a96cea8ae22ead2852a2b
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-07-30 16:23:15 +0100

    Returning value of final score and adding back button

--- src/pages/FinalScreen.js
+++ src/pages/FinalScreen.js
@@ -1,10 +1,26 @@
+import {Box, Button, Typography} from "@mui/material";
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { handleAmountChange, handleScoreChange } from '../redux/actions';
 
 const FinalScreen = () => {
+    const { score } = useSelector(state => state )
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleBackToSettings = () =>{
+        dispatch(handleScoreChange(0));
+        dispatch(handleAmountChange(50));
+        history.push("/")
+    }
     return (
-        <div>
-           Final Screen
-        </div>
+
+        <Box mt={30}>
+            <Typography variant="h3" fontWeight="bold" mb={3}>Your final score is {score}</Typography>
+            <Button onClick={handleBackToSettings} variant="outlined">Try again!</Button>
+
+        </Box>
     );
 };
 
