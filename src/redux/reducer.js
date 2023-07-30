97915e3c6e06e87e86baefaef2da9709fd6912c5
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-07-30 10:42:49 +0100

    setting initial state for reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    question_category: "",
+    question_difficulty: "",
+    question_type:"",
+    amount_of_question: 50,
+    score: 0,
+};
+
+const action =()=>{}
+
+const reducer = (state = initialState, action) =>{
+
+}
