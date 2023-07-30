e5f493416282066f3ed493b12b0f29753c2a9085
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-07-30 14:26:15 +0100

    setting API responses for properties of question

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -11,12 +11,21 @@ const Questions = () => {
         question_type,
         amount_of_question,
     } = useSelector((state) => state)
-    console.log(amount_of_question)
+    console.log(question_category,question_difficulty , question_type, amount_of_question)
 
 
-    let apiUrl = `/api.php?amount=10`
-
+    let apiUrl = `/api.php?amount=${amount_of_question}`
+    if(question_category) {
+        apiUrl = apiUrl.concat(`&category=${question_category}`)
+    }
+    if(question_difficulty) {
+        apiUrl = apiUrl.concat(`&difficulty=${question_difficulty}`)
+    }
+    if(question_type) {
+        apiUrl = apiUrl.concat(`&type=${question_type}`)
+    }
     const { response, loading } = useAxios({ url: apiUrl })
+
     console.log(response)
     return (
         (<Box>
